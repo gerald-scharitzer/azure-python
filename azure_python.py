4b8072400f1046d775278a4c580a41479ef243b6
gerald-scharitzer <gerald.scharitzer@gmail.com>
2023-11-16 00:43:53 +0100

    headers

--- azure_python.py
+++ azure_python.py
@@ -6,12 +6,17 @@ from yaml import safe_load
 def main():
     print("Azure Python")
     conf = safe_load(stdin)
-    uri = conf["uri"]
-    response = get(uri)
+    subscriptionId = conf["subscriptionId"]
+    uri = "https://management.azure.com/subscriptions/" + subscriptionId + \
+        "/locations?api-version=2022-12-01"
+    token = conf["token"]
+    headers = { "Authorization": "Bearer " + token}
+    response = get(uri, headers=headers)
     print(response)
     print(USAGE)
 
-USAGE = """usage: python azure_python.py
+USAGE = """\
+usage: python azure_python.py
 where
   - "python" is the Python runtime
 """
