26600c0cf03d1f2b24a7e794be3b5d64c225f74e
gerald-scharitzer <gerald.scharitzer@gmail.com>
2023-11-18 20:38:42 +0100

    token

--- azure_python.py
+++ azure_python.py
@@ -1,30 +1,57 @@
 from sys import stdin
 
-from requests import codes, get
+from requests import codes, get, post
 from yaml import safe_load
-
-def main():
-    print("Azure Python")
-    conf = safe_load(stdin)
-    subscriptionId = conf["subscriptionId"]
-    uri = "https://management.azure.com/subscriptions/" + subscriptionId + \
-        "/locations?api-version=2022-12-01"
-    token = conf["token"]
-    headers = { "Authorization": "Bearer " + token}
-    response = get(uri, headers=headers)
-    if response.status_code == codes.OK:
-        print(response)
-    else:
-        request = response.request
-        print(request.method + " " + request.url)
-        print(str(response.status_code) + " " + response.reason)
-    print(USAGE)
 
 USAGE = """\
 usage: python azure_python.py
 where
   - "python" is the Python runtime
 """
+
+def main():
+    HOST_LOGIN = "https://login.microsoftonline.com/"
+    HOST_MANAGE = "https://management.azure.com/"
+    print("Azure Python")
+    conf = safe_load(stdin)
+
+    access_token = conf["access_token"]
+    if (len(access_token) == 0):
+        tenant_id = conf["tenant_id"]
+        client_id = conf["client_id"]
+        secret = conf["secret"]
+        uri = HOST_LOGIN + tenant_id + "/oauth2/v2.0/token"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "client_id": client_id,
+            "scope": HOST_MANAGE + ".default",
+            "client_secret": secret,
+            "grant_type": "client_credentials"
+        }
+        response = post(uri, data, headers=headers)
+        if response.status_code == codes.OK:
+            # TODO check token_type Bearer
+            print(response.text)
+        else:
+            request = response.request
+            print(request.method + " " + request.url)
+            print(str(response.status_code) + " " + response.reason)
+            print(response.text)
+
+    subscription_id = conf["subscription_id"]
+    uri = HOST_MANAGE + "subscriptions/" + subscription_id + \
+        "/locations?api-version=2022-12-01"
+    headers = {"Authorization": "Bearer " + access_token}
+    response = get(uri, headers=headers)
+    if response.status_code == codes.OK:
+        print(response.text)
+    else:
+        request = response.request
+        print(request.method + " " + request.url)
+        print(str(response.status_code) + " " + response.reason)
+        print(response.text)
+    
+    print(USAGE)
 
 if __name__ == "__main__":
     main()
